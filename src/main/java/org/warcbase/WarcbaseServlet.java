04bf522cbbf40c8a4cf6065518c87c62dd59bd7a
milad621 <milad621@gmail.com>
2013-07-27 03:26:50 -0400

    debug unicode

--- src/main/java/org/warcbase/WarcbaseServlet.java
+++ src/main/java/org/warcbase/WarcbaseServlet.java
@@ -30,13 +30,15 @@ public class WarcbaseServlet extends HttpServlet
     		//IOUtils.write(ResponseRecord.getBodyByte(data), resp.getOutputStream());
 		}
 		else{
-			//PrintWriter out = resp.getWriter();
+		  System.setProperty("file.encoding", "UTF8");
+			PrintWriter out = resp.getWriter();
+			//out.
 			TextDocument2 t2 = new TextDocument2(null, null, null);
 			String bodyContent = new String(ResponseRecord.getBodyByte(data), "UTF8");
 			bodyContent = t2.fixURLs(bodyContent, query, d);
 			//System.out.println(bodyContent);
-			//out.println(bodyContent);
-			resp.getOutputStream().write(ResponseRecord.getBodyByte(data));
+			out.println(bodyContent);
+			//resp.getOutputStream().write(ResponseRecord.getBodyByte(data));
 		}
 	}
 	
