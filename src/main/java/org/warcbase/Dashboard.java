e361ca8e56c71af0074dc80826eb74c118bd2cdf
milad621 <milad621@gmail.com>
2013-07-28 22:51:58 -0400

    Dashboard

--- src/main/java/org/warcbase/Dashboard.java
+++ src/main/java/org/warcbase/Dashboard.java
@@ -1,6 +1,9 @@
 package org.warcbase;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
@@ -9,6 +12,7 @@ import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.filter.FirstKeyOnlyFilter;
+import org.hsqldb.lib.Iterator;
 
 public class Dashboard {
   public static Configuration hbaseConfig = null;
@@ -19,9 +23,9 @@ public class Dashboard {
   }
   
   public static String getFileType(String url){
-    System.out.println(url);
+    //System.out.println(url);
     String[] splits = url.split("\\.");
-    System.out.println(splits.length);
+    //System.out.println(splits.length);
     if(splits.length <= 1)
       return "";
     return splits[splits.length - 1];
@@ -46,14 +50,27 @@ public class Dashboard {
       // TODO Auto-generated catch block
       e.printStackTrace();
     }
+    HashMap<String, Integer> fileTypeCounter = null;
+    
     for (Result rr = scanner.next(); rr != null && count < 200; rr = scanner.next()) {
       byte[] key = rr.getRow();
       String url = new String(key, "UTF8");
       count++;
       if(url.length() > 10)
         url = url.substring(url.length() - 10);
-      System.out.println(new String(key, "UTF8") + getFileType(url));
+      String fileType = getFileType(url);
+      if(fileTypeCounter.containsKey(fileType))
+        fileTypeCounter.put(fileType, fileTypeCounter.get(fileType) + 1);
+      else
+        fileTypeCounter.put(fileType, 1);
+      //System.out.println(new String(key, "UTF8") + getFileType(url));
     }
     System.out.println(count);
+    SortedSet<String> sortedKeys = new TreeSet<String>(fileTypeCounter.keySet());
+    Iterator it = (Iterator) sortedKeys.iterator();
+    while(it.hasNext()){
+      String value=(String)it.next();
+      System.out.println(value + " " + fileTypeCounter.get(value));
+    }
   }
 }
