944b138ee0d2152253a6407a9cd2c395de62f558
milad621 <milad621@gmail.com>
2013-12-01 17:01:51 -0500

    started a new branch to extract text from warcbase data and organize the data to run topic modeling

--- src/main/java/org/warcbase/data/Util.java
+++ src/main/java/org/warcbase/data/Util.java
@@ -45,6 +45,17 @@ public class Util {
       newhost.append(":" + port);
     return newhost.toString();
   }
+  
+  public static String reverseBacUri(String reverse) {
+    String domain = Util.getDomain(reverse);
+    domain = Util.reverseBacHostnamek(domain);
+    String[] splits = reverse.split("\\/");
+    if(splits.length < 2){
+      return domain;
+    }
+    String file = reverse.substring(splits[0].length());
+    return domain + file;
+  }
 
   public static String getFileType(String url) {
     if (url.length() > 0 && url.charAt(url.length() - 1) == '/')
@@ -68,38 +79,17 @@ public class Util {
     return splits[0];
   }
 
-  public static void main(String[] args) {
-    System.out.println(Util.reverseHostname("http://www.boxer.senate.gov/"));// http://www.ayotte.senate.gov/
-    System.out.println(Util
-        .reverseHostname("https://tibanna.umiacs.umd.edu:9191/warc_congress108/all"));
-    String host = Util.reverseHostname("https://tibanna.umiacs.umd.edu/warc_congress108/all");
-    String[] splits = host.split("\\/");
-    String[] parts = splits[0].split("\\.", 0);
-    StringBuilder newhost = new StringBuilder();
-    newhost.append("http://");
-    String[] ports = splits[0].split("\\:", 0);
-    String port = null;
-    if (ports.length > 1)
-      port = ports[1];
-    parts = ports[0].split("\\.", 0);
-    if (parts.length - 1 > 0) {
-
-      System.out.println(ports.length);
-      System.out.println(ports[0]);
-      // if(ports.length > 1)
-      // newhost.append(ports[0]);
-
-      // else
-      // newhost.app
-    }
-    for (int i = parts.length - 1; i > 0; i--) {
-      if (i > 0)
-        newhost.append(parts[i]).append(".");
-    }
-    newhost.append(parts[0]);
-    if (port != null)
-      newhost.append(":" + port);
-    System.out.println(Util.reverseBacHostnamek(host));
+  public static void main(String[] args) throws MalformedURLException {
+    String uri = "http://www.house.gov/mthompson/the_1st_district.htm";
+    System.out.println(Util.reverseHostname(uri));// http://www.ayotte.senate.gov/
+    String reverse = Util.reverseHostname("http://www.house.gov/mthompson/the_1st_district.htm");
+    URL url = new URL(uri);
+    String host = url.getHost();
+    System.out.println(host);
+    String domain = Util.getDomain(reverse);
+    domain = Util.reverseBacHostnamek(domain);
+    System.out.println(domain);
+    System.out.println(Util.reverseBacUri(reverse));
   }
 
   public static String getUriExtension(String thisTargetURI) {
