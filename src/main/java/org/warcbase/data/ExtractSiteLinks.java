a17e3c74954a9fe380c16aa1e9bf7c7ebbaa64f2
Jeffyrao <raojinfeng@gmail.com>
2014-05-29 14:13:43 -0400

    fix issue 30, add ExtractSiteLinks code

--- src/main/java/org/warcbase/data/ExtractSiteLinks.java
+++ src/main/java/org/warcbase/data/ExtractSiteLinks.java
@@ -1,20 +1,19 @@
 package org.warcbase.data;
 
-import it.unimi.dsi.fastutil.ints.Int2ObjectAVLTreeMap;
-import it.unimi.dsi.fastutil.ints.IntArrayList;
-import it.unimi.dsi.fastutil.objects.ObjectArrayList;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -23,187 +22,267 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
-import org.apache.commons.io.FileUtils;
-import org.warcbase.ingest.IngestFiles;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.mapreduce.Counters;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.jwat.arc.ArcRecordBase;
+import org.warcbase.data.PrefixMapping.PrefixNode;
+import org.warcbase.mapreduce.ArcInputFormat;
 
-import au.com.bytecode.opencsv.CSVReader;
+public class ExtractSiteLinks extends Configured implements Tool{
+  private static final Logger LOG = Logger.getLogger(ExtractSiteLinks.class);
 
-public class ExtractSiteLinks {
-  
-  public class PrefixNode{
-    int id;
-    String url;
-    Long startPos;
-    Long endPos;
-    
-    public PrefixNode(int id, String url, Long startPos, Long endPos){
-      this.id = id;
-      this.url = url;
-      this.startPos = startPos;
-      this.endPos = endPos;
-    }
-    
-    public int getId(){ return id; }
-    public String getUrl(){ return url; }
-    public Long getStartPos(){ return startPos; }
-    public Long getEndPos(){ return endPos; }
-  }
-  
-  static final String FST = "fstfile";
-  static final String PREFIX = "prefixfile";
-  static final String LINKS = "linkdir";
-  static final String OUTPUT = "output";
-  
-  static UriMapping map;
-  static ArrayList<PrefixNode> prefixes;
-  static Int2ObjectAVLTreeMap<IntArrayList> links;
-  static int [][] prefixLinks;
-  
-  private static void loadPrefix(String prefixFile) throws IOException{
-    ExtractSiteLinks instance = new ExtractSiteLinks();
-    final Comparator<PrefixNode> comparator = new Comparator<PrefixNode>(){
-      @Override
-      public int compare(PrefixNode n1, PrefixNode n2){
-        if(n1.startPos > n2.startPos){
-          return 1;
-        }else if( n1.startPos == n2.startPos ){
-          return 0;
-        }else{
-          return -1;
-        }
+  private static enum Records {
+    TOTAL, LINK_COUNT
+  };
+
+  public static class ExtractSiteLinksMapper extends
+      Mapper<LongWritable, ArcRecordBase, IntWritable, IntWritable> {
+    private IntWritable urlNode = new IntWritable();
+    private static UriMapping fst;
+    private static PrefixMapping prefixMap;
+    private static ArrayList<PrefixNode> prefix;
+
+    @Override
+    public void setup(Context context) {
+      try {
+        Configuration conf = context.getConfiguration();
+        Path[] localFiles = DistributedCache.getLocalCacheFiles(conf);
+
+        // load FST UriMapping from file
+        fst = (UriMapping) Class.forName(conf.get("UriMappingClass")).newInstance();
+        fst.loadMapping(localFiles[0].toString());// simply assume only one file in distributed
+                                                  // cache
+        prefixMap = (PrefixMapping) Class.forName(conf.get("PrefixMappingClass")).newInstance();
+        prefix = prefixMap.loadPrefix(localFiles[1].toString(), fst);
+      
+      } catch (Exception e) {
+        e.printStackTrace();
+        throw new RuntimeException("Error Initializing UriMapping");
       }
-    };
-    prefixes = new ArrayList<PrefixNode>();
-    CSVReader reader = new CSVReader(new FileReader(prefixFile),',');
-    reader.readNext();
-    String line;
-    String[] record = null;
-    while((record = reader.readNext())!=null){
-      int id = Integer.valueOf(record[0]);
-      String url = record[1];
-      List<String> results = map.prefixSearch(url);
-      Long[] boundary = map.getIdRange(results);
-      PrefixNode node = instance.new PrefixNode(id, url, boundary[0], boundary[1]);
-      prefixes.add(node);
     }
-    Collections.sort(prefixes,comparator);
-    prefixLinks = new int[prefixes.size()+1][prefixes.size()+1];
-    reader.close();
-  }
-  
-  private static void loadLinks(String linkDir) throws IOException{
-    links = new Int2ObjectAVLTreeMap<IntArrayList>();
-    File dataDir  = new File(linkDir);
-    File[] files = dataDir.listFiles();
-    for(File file:files){
-      if(!file.getName().startsWith("part"))
-        continue;
-      String contents = FileUtils.readFileToString(file);
-      String[] lines = contents.split("\\n");
-      for(String line: lines){
-        // This need to modify according to your input file
-        line = line.replace('[', ' ');
-        line = line.replace(']', ' ');
-        line = line.replace(',', ' ');
-        if (!line.equals("")) { // non-empty string 
-          line.trim();
-          String[] nodes = line.split("\\s+");
-          IntArrayList list = new IntArrayList();
-          for(int i=1; i<nodes.length; i++){
-            list.add(Integer.valueOf(nodes[i]));
+
+    @Override
+    public void map(LongWritable key, ArcRecordBase record, Context context) throws IOException,
+        InterruptedException {
+
+      context.getCounter(Records.TOTAL).increment(1);
+      String url = record.getUrlStr();
+      String type = record.getContentTypeStr();
+      Date date = record.getArchiveDate();
+      DateFormat df = new SimpleDateFormat("yyyyMMdd");
+      String time = df.format(date);
+      InputStream content = record.getPayloadContent();
+      
+      System.out.println(time);
+      if(beginDate != null && endDate != null){
+        if(time.compareTo(beginDate)<0 || time.compareTo(endDate)>0)
+          return;
+      }else if(beginDate == null && endDate != null){
+        if(time.compareTo(endDate)>0)
+          return;
+      }else if(beginDate != null && endDate == null){
+        if(time.compareTo(beginDate)<0)
+          return;
+      }
+      
+      if (!type.equals("text/html"))
+        return;
+      Document doc = Jsoup.parse(content, "ISO-8859-1", url); // parse in ISO-8859-1 format
+      Elements links = doc.select("a[href]"); // empty if none match
+
+      if (fst.getID(url) != -1 && prefixMap.getPrefixId(fst.getID(url), prefix)!=-1) { // the url is already indexed in UriMapping
+        int prefixSourceId = prefixMap.getPrefixId(fst.getID(url), prefix);
+        urlNode.set(prefixSourceId);
+        Set<Integer> linkUrlSet = new HashSet<Integer>();
+        if (links != null) {
+          for (Element link : links) {
+            String linkUrl = link.attr("abs:href");
+            if (fst.getID(linkUrl) != -1 && prefixMap.getPrefixId(fst.getID(linkUrl), prefix)!=-1) { // linkUrl is already indexed
+              int prefixTargetId = prefixMap.getPrefixId(fst.getID(linkUrl), prefix);
+              linkUrlSet.add(prefixTargetId);
+            }
           }
-          links.put(Integer.valueOf(nodes[0]), list);
-        }
+          boolean emitFlag = false;
+          for (Integer linkID : linkUrlSet) {
+            context.write(urlNode,new IntWritable(linkID));
+          }
+      
+        } 
       }
-      System.out.println("Init links of "+file.getName());
     }
   }
   
-  private static int getPrefixId(int id){
-    int start = 0, end = prefixes.size()-1;
-    int mid;
-    while(start<=end){
-      mid = (start+end)/2;
-      if(prefixes.get(mid).getStartPos() <= id && prefixes.get(mid).getEndPos() >= id){
-        return prefixes.get(mid).getId();
-      }else if(prefixes.get(mid).getStartPos() > id){
-        end = mid-1;
+  private static class ExtractSiteLinksReducer extends
+  Reducer<IntWritable, IntWritable, IntWritable, Text> {
+  @Override
+  public void reduce(IntWritable key, Iterable<IntWritable> values, Context context)
+      throws IOException, InterruptedException{
+    Map<Integer,Integer> links = new HashMap<Integer,Integer>();
+    //remove duplicate links
+    for(IntWritable value : values){
+      if(links.containsKey(value.get())){
+        //increment 1 link count
+        links.put(value.get(),links.get(value.get())+1);
       }else{
-        start = mid+1;
+        links.put(value.get(), 1);
       }
     }
-    return -1;
-  }
-  
-  private static void saveSiteLinks(String output) throws IOException{
-    BufferedWriter writer = new BufferedWriter(new FileWriter(output));
-    writer.write("Source,Target,Weight\n");
-    for(int src=1; src<=prefixes.size();src++){
-      for(int dest=1; dest<=prefixes.size();dest++){
-        if(prefixLinks[src][dest]!=0 && src!=dest){
-          writer.write(src+","+dest+","+prefixLinks[src][dest]+"\n");
-        }
-      }
+    for(Entry<Integer,Integer> link: links.entrySet()){
+      context.getCounter(Records.LINK_COUNT).increment(1);
+      String outputValue = String.valueOf(link.getKey()) +"   "+ String.valueOf(link.getValue());
+      context.write(key, new Text(outputValue));
     }
-    writer.close();
   }
+}
   
-  public static void main(String[] args) throws IOException {
-    // TODO Auto-generated method stub
-    
+  /**
+   * Creates an instance of this tool.
+   */
+  public ExtractSiteLinks() {}
+
+  private static final String INPUT = "input";
+  private static final String OUTPUT = "output";
+  private static final String URI_MAPPING = "uriMapping";
+  private static final String PREFIX_FILE = "prefixFile";
+  private static final String NUM_REDUCERS = "numReducers";
+  private static final String BEGIN="begin";
+  private static final String END="end";
+  private static String beginDate=null, endDate=null;
+
+  /**
+   * Runs this tool.
+   */
+  @SuppressWarnings({ "static-access" })
+  public int run(String[] args) throws Exception {
     Options options = new Options();
 
+    options.addOption(OptionBuilder.withArgName("path").hasArg().withDescription("input path")
+        .create(INPUT));
+    options.addOption(OptionBuilder.withArgName("path").hasArg().withDescription("output path")
+        .create(OUTPUT));
     options.addOption(OptionBuilder.withArgName("path").hasArg()
-        .withDescription("FST data file").create(FST));
+        .withDescription("uri mapping file path").create(URI_MAPPING));
     options.addOption(OptionBuilder.withArgName("path").hasArg()
-        .withDescription("prefix file").create(PREFIX));
-    options.addOption(OptionBuilder.withArgName("path").hasArg()
-        .withDescription("links dir").create(LINKS));
-    options.addOption(OptionBuilder.withArgName("path").hasArg()
-        .withDescription("extracted site links").create(OUTPUT));
-    
-    CommandLine cmdline = null;
+        .withDescription("prefix mapping file path").create(PREFIX_FILE));
+    options.addOption(OptionBuilder.withArgName("num").hasArg()
+        .withDescription("number of reducers").create(NUM_REDUCERS));
+    options.addOption(OptionBuilder.withArgName("path").hasArg().withDescription("begin date (optional)")
+        .create(BEGIN));
+    options.addOption(OptionBuilder.withArgName("path").hasArg().withDescription("end date (optional)")
+        .create(END));
+
+    CommandLine cmdline;
     CommandLineParser parser = new GnuParser();
 
     try {
       cmdline = parser.parse(options, args);
     } catch (ParseException exp) {
-      System.err.println("Error parsing command line: "
-          + exp.getMessage());
-      System.exit(-1);
+      System.err.println("Error parsing command line: " + exp.getMessage());
+      return -1;
     }
-    
-    if (!cmdline.hasOption(FST) || !cmdline.hasOption(PREFIX)
-        || !cmdline.hasOption(LINKS) || !cmdline.hasOption(OUTPUT) ) {
+
+    if (!cmdline.hasOption(INPUT) || !cmdline.hasOption(OUTPUT) 
+        || !cmdline.hasOption(URI_MAPPING) || !cmdline.hasOption(PREFIX_FILE)) {
+      System.out.println("args: " + Arrays.toString(args));
       HelpFormatter formatter = new HelpFormatter();
-      formatter.printHelp(IngestFiles.class.getCanonicalName(), options);
-      System.exit(-1);
+      formatter.setWidth(120);
+      formatter.printHelp(this.getClass().getName(), options);
+      ToolRunner.printGenericCommandUsage(System.out);
+      return -1;
+    }
+
+    String inputPath = cmdline.getOptionValue(INPUT);
+    String outputPath = cmdline.getOptionValue(OUTPUT);
+    String mappingPath = cmdline.getOptionValue(URI_MAPPING);
+    String prefixFile = cmdline.getOptionValue(PREFIX_FILE);
+    int reduceTasks = cmdline.hasOption(NUM_REDUCERS) ? Integer.parseInt(cmdline
+        .getOptionValue(NUM_REDUCERS)) : 1;
+
+    LOG.info("Tool: " + ExtractSiteLinks.class.getSimpleName());
+    LOG.info(" - input path: " + inputPath);
+    LOG.info(" - output path: " + outputPath);
+    LOG.info(" - mapping file path:" + mappingPath);
+    LOG.info(" - prefix file path:" + prefixFile);
+    LOG.info(" - number of reducers: " + reduceTasks);
+    if(cmdline.hasOption(BEGIN)){
+      beginDate = cmdline.getOptionValue(BEGIN);
+      LOG.info(" - begin date: " + beginDate);
+    }
+    if(cmdline.hasOption(END)){
+      endDate = cmdline.getOptionValue(END);
+      LOG.info(" - end date: " + endDate);
     }
     
-    String fstFile = cmdline.getOptionValue(FST);
-    String prefixFile = cmdline.getOptionValue(PREFIX);
-    String linkDir = cmdline.getOptionValue(LINKS);
-    String output = cmdline.getOptionValue(OUTPUT);
     
-    map = new UriMapping(fstFile);
-    loadPrefix(prefixFile);
-    loadLinks(linkDir);
-    
-    for(PrefixNode node: prefixes){
-      for(Long iterId=node.getStartPos(); iterId <= node.getEndPos(); iterId++){
-        IntArrayList iterLinks = links.get(iterId.intValue());
-        if(iterLinks == null) continue;
-        for(int iterLink: iterLinks){
-          int dest_id = getPrefixId(iterLink);
-          if(dest_id != -1){
-            int src_id = node.getId();
-            prefixLinks[src_id][dest_id] += 1;
-          }
-        }
-      }
-    }
-    
-    saveSiteLinks(output);
+    Job job = new Job(getConf(), ExtractSiteLinks.class.getSimpleName());
+    job.setJarByClass(ExtractSiteLinks.class);
+
+    // Pass in the class name as a String; this is makes the mapper general
+    // in being able to load any collection of Indexable objects that has
+    // url_id/url mapping specified by a UriMapping object.
+    job.getConfiguration().set("UriMappingClass", UriMapping.class.getCanonicalName());
+    job.getConfiguration().set("PrefixMappingClass",PrefixMapping.class.getCanonicalName());
+    // Put the mapping file in the distributed cache so each map worker will
+    // have it.
+    DistributedCache.addCacheFile(new URI(mappingPath), job.getConfiguration());
+    DistributedCache.addCacheFile(new URI(prefixFile), job.getConfiguration());
+
+    job.setNumReduceTasks(reduceTasks); // no reducers
+
+    FileInputFormat.setInputPaths(job, new Path(inputPath));
+    FileOutputFormat.setOutputPath(job, new Path(outputPath));
+
+    job.setInputFormatClass(ArcInputFormat.class);
+    // set map (key,value) output format
+    job.setMapOutputKeyClass(IntWritable.class);
+    job.setMapOutputValueClass(IntWritable.class);
+    job.setOutputKeyClass(IntWritable.class);
+    job.setOutputValueClass(Text.class);
+
+    job.setMapperClass(ExtractSiteLinksMapper.class);
+    job.setReducerClass(ExtractSiteLinksReducer.class);
+
+    // Delete the output directory if it exists already.
+    Path outputDir = new Path(outputPath);
+    FileSystem.get(job.getConfiguration()).delete(outputDir, true);
+
+    long startTime = System.currentTimeMillis();
+    job.waitForCompletion(true);
+    LOG.info("Job Finished in " + (System.currentTimeMillis() - startTime) / 1000.0 + " seconds");
+
+    Counters counters = job.getCounters();
+    int numRecords = (int) counters.findCounter(Records.TOTAL).getValue();
+    int numLinks = (int) counters.findCounter(Records.LINK_COUNT).getValue();
+    LOG.info("Read " + numRecords + " records.");
+    LOG.info("Extracts " + numLinks + " links.");
+
+    return 0;
   }
 
+  /**
+   * Dispatches command-line arguments to the tool via the {@code ToolRunner}.
+   */
+  public static void main(String[] args) throws Exception {
+    ToolRunner.run(new ExtractSiteLinks(), args);
+  }
 }
---
+++ src/main/java/org/warcbase/data/PrefixMapping.java
@@ -0,0 +1,78 @@
+package org.warcbase.data;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import au.com.bytecode.opencsv.CSVReader;
+public class PrefixMapping{
+  public class PrefixNode {
+    int id;
+    String url;
+    Long startPos;
+    Long endPos;
+    
+    public PrefixNode(int id, String url, Long startPos, Long endPos){
+      this.id = id;
+      this.url = url;
+      this.startPos = startPos;
+      this.endPos = endPos;
+    }
+    
+    public int getId(){ return id; }
+    public String getUrl(){ return url; }
+    public Long getStartPos(){ return startPos; }
+    public Long getEndPos(){ return endPos; }
+  }
+  
+  public static ArrayList<PrefixNode> loadPrefix(String prefixFile, UriMapping map) throws IOException{
+    PrefixMapping instance = new PrefixMapping();
+    final Comparator<PrefixNode> comparator = new Comparator<PrefixNode>(){
+      @Override
+      public int compare(PrefixNode n1, PrefixNode n2){
+        if(n1.startPos > n2.startPos){
+          return 1;
+        }else if( n1.startPos == n2.startPos ){
+          return 0;
+        }else{
+          return -1;
+        }
+      }
+    };
+    ArrayList<PrefixNode> prefixes = new ArrayList<PrefixNode>();
+    CSVReader reader = new CSVReader(new FileReader(prefixFile),',');
+    reader.readNext();
+    String line;
+    String[] record = null;
+    while((record = reader.readNext())!=null){
+      int id = Integer.valueOf(record[0]);
+      String url = record[1];
+      List<String> results = map.prefixSearch(url);
+      Long[] boundary = map.getIdRange(results);
+      PrefixNode node = instance.new PrefixNode(id, url, boundary[0], boundary[1]);
+      prefixes.add(node);
+    }
+    Collections.sort(prefixes,comparator);
+    reader.close();
+    return prefixes;
+  }
+  
+  public int getPrefixId(int id, ArrayList<PrefixNode> prefixes){
+    int start = 0, end = prefixes.size()-1;
+    int mid;
+    while(start<=end){
+      mid = (start+end)/2;
+      if(prefixes.get(mid).getStartPos() <= id && prefixes.get(mid).getEndPos() >= id){
+        return prefixes.get(mid).getId();
+      }else if(prefixes.get(mid).getStartPos() > id){
+        end = mid-1;
+      }else{
+        start = mid+1;
+      }
+    }
+    return -1;
+  }
+}
