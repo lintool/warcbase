a8953616248cd962db9f9b70d0de3354a503a9bc
lintool <jimmylin@umd.edu>
2014-06-04 15:12:40 -0400

    Refactoring, added test case (currently broken).

--- src/test/java/org/warcbase/data/UriMappingTest.java
+++ src/test/java/org/warcbase/data/UriMappingTest.java
@@ -24,8 +24,8 @@ public class UriMappingTest {
 
   @Before
   public void setUp() throws Exception {
-    String inputValues[] = {"cat", "catch","cut", "doga","dogb","dogs"};
-    long outputValues[] = {1,2,3,4,5,6};
+    String inputValues[] = { "cat", "catch", "cut", "doga", "dogb", "dogs" };
+    long outputValues[] = { 1, 2, 3, 4, 5, 6 };
     
     PositiveIntOutputs outputs = PositiveIntOutputs.getSingleton();
     Builder<Long> builder = new Builder<Long>(INPUT_TYPE.BYTE1, outputs);
@@ -38,20 +38,64 @@ public class UriMappingTest {
     FST<Long> fst = builder.finish();
     map = new UriMapping(fst);
   }
-  
+
   @Test
-  public void testPrefixSearch() throws Exception {
-    List<String> results = map.prefixSearch("dog");
-    assertTrue(results.size()==3);
-    assertEquals("doga",results.get(0));
-    assertEquals("dogb",results.get(1));
-    assertEquals("dogs",results.get(2));
+  public void testGetIds() {
+    assertEquals(-1, map.getID("apple"));
+    assertEquals(1, map.getID("cat"));
+    assertEquals(2, map.getID("catch"));
+    assertEquals(3, map.getID("cut"));
+    assertEquals(-1, map.getID("cuttery"));
+    assertEquals(4, map.getID("doga"));
+    assertEquals(5, map.getID("dogb"));
+    assertEquals(6, map.getID("dogs"));
+    assertEquals(-1, map.getID("dogz"));
   }
-  
+
+  @Test
+  public void testGetUrls() {
+    assertEquals(null, map.getUrl(0));
+    assertEquals("cat", map.getUrl(1));
+    assertEquals("catch", map.getUrl(2));
+    assertEquals("cut", map.getUrl(3));
+    assertEquals("doga", map.getUrl(4));
+    assertEquals("dogb", map.getUrl(5));
+    assertEquals("dogs", map.getUrl(6));
+    assertEquals(null, map.getUrl(7));
+  }
+
+  @Test
+  public void testPrefixSearch() {
+    List<String> results;
+
+    results = map.prefixSearch("cut");
+    assertEquals(1, results.size());
+    assertEquals("cut", results.get(0));
+
+    results = map.prefixSearch("dog");
+    assertEquals(3, results.size());
+    assertEquals("doga", results.get(0));
+    assertEquals("dogb", results.get(1));
+    assertEquals("dogs", results.get(2));
+
+    results = map.prefixSearch("");
+    assertEquals(0, results.size());
+
+    results = map.prefixSearch(null);
+    assertEquals(0, results.size());
+
+    // Broken test case.
+    results = map.prefixSearch("dad");
+    assertEquals(1, results.size());
+  }
+
   @Test
   public void testGetIdRange() throws IOException{
-    List<String> results = map.prefixSearch("dog");
-    Long[] range = map.getIdRange(results);
+    List<String> results;
+    Long[] range;
+
+    results = map.prefixSearch("dog");
+    range = map.getIdRange(results);
     assertTrue(range[0] == 4);
     assertTrue(range[1] == 6);
   }
