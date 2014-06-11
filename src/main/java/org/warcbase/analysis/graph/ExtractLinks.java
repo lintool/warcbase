b02f33c9b43a12c3005110041cf44a781224b41f
lintool <jimmylin@umd.edu>
2014-06-11 16:36:39 -0400

    Refactoring, code cleanup.

--- src/main/java/org/warcbase/analysis/graph/ExtractLinks.java
+++ src/main/java/org/warcbase/analysis/graph/ExtractLinks.java
@@ -1,21 +1,14 @@
 package org.warcbase.analysis.graph;
 
+import it.unimi.dsi.fastutil.ints.IntAVLTreeSet;
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringWriter;
-import java.net.SocketException;
-import java.net.SocketTimeoutException;
-import java.net.URI;
-import java.net.UnknownHostException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
-import java.util.StringTokenizer;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -26,43 +19,31 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.filecache.DistributedCache;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.mapreduce.Counters;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
-import org.eclipse.jetty.util.log.Log;
-import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
-import org.jsoup.UnsupportedMimeTypeException;
-import org.jsoup.helper.Validate;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.jwat.arc.ArcRecordBase;
-import org.jwat.common.HttpHeader;
 import org.warcbase.data.UriMapping;
 import org.warcbase.mapreduce.ArcInputFormat;
 
-import java.util.Arrays;
+import com.google.common.base.Joiner;
 
 /**
- * Extract Links demo.
- * 
- * @author Jinfeng Rao modified code based on CountTrecDocuments.java by Jimmy Lin
+ * Program for extracting links from ARC files.
  */
 public class ExtractLinks extends Configured implements Tool {
   private static final Logger LOG = Logger.getLogger(ExtractLinks.class);
@@ -72,21 +53,30 @@ public class ExtractLinks extends Configured implements Tool {
   };
 
   public static class ExtractLinksMapper extends
-      Mapper<LongWritable, ArcRecordBase, IntWritable, List> {
-    private IntWritable urlNode = new IntWritable();
-    private List linkNodes;
+      Mapper<LongWritable, ArcRecordBase, IntWritable, Text> {
+    private static final DateFormat df = new SimpleDateFormat("yyyyMMdd");
     private static UriMapping fst;
+
+    private static final Joiner JOINER = Joiner.on(",");
+    private static final IntWritable KEY = new IntWritable();
+    private static final Text VALUE = new Text();
 
     @Override
     public void setup(Context context) {
       try {
         Configuration conf = context.getConfiguration();
-        Path[] localFiles = DistributedCache.getLocalCacheFiles(conf);
+        // There appears to be a bug in getCacheFiles() which returns null,
+        // even though getLocalCacheFiles is deprecated...
+        @SuppressWarnings("deprecation")
+        Path[] localFiles = context.getLocalCacheFiles();
+
+        LOG.info("cache contents: " + Arrays.toString(localFiles));
+        System.out.println("cache contents: " + Arrays.toString(localFiles));
 
         // load FST UriMapping from file
         fst = (UriMapping) Class.forName(conf.get("UriMappingClass")).newInstance();
-        fst.loadMapping(localFiles[0].toString());// simply assume only one file in distributed
-                                                  // cache
+        fst.loadMapping(localFiles[0].toString());
+        // simply assume only one file in distributed cache.
       } catch (Exception e) {
         e.printStackTrace();
         throw new RuntimeException("Error Initializing UriMapping");
@@ -94,59 +84,58 @@ public class ExtractLinks extends Configured implements Tool {
     }
 
     @Override
-    public void map(LongWritable key, ArcRecordBase record, Context context) throws IOException,
-        InterruptedException {
-
+    public void map(LongWritable key, ArcRecordBase record, Context context)
+        throws IOException, InterruptedException {
       context.getCounter(Records.TOTAL).increment(1);
       String url = record.getUrlStr();
       String type = record.getContentTypeStr();
       Date date = record.getArchiveDate();
-      DateFormat df = new SimpleDateFormat("yyyyMMdd");
       String time = df.format(date);
       InputStream content = record.getPayloadContent();
-      
-      if(beginDate != null && endDate != null){
-        if(time.compareTo(beginDate)<0 || time.compareTo(endDate)>0)
+
+      if (beginDate != null && endDate != null) {
+        if (time.compareTo(beginDate) < 0 || time.compareTo(endDate) > 0) {
           return;
-      }else if(beginDate == null && endDate != null){
-        if(time.compareTo(endDate)>0)
+        }
+      } else if (beginDate == null && endDate != null) {
+        if (time.compareTo(endDate) > 0) {
           return;
-      }else if(beginDate != null && endDate == null){
-        if(time.compareTo(beginDate)<0){
+        }
+      } else if (beginDate != null && endDate == null) {
+        if (time.compareTo(beginDate) < 0) {
           return;
         }
       }
-      
-      if (!type.equals("text/html"))
+
+      if (!type.equals("text/html")) {
         return;
+      }
+
       Document doc = Jsoup.parse(content, "ISO-8859-1", url); // parse in ISO-8859-1 format
       Elements links = doc.select("a[href]"); // empty if none match
 
       if (fst.getID(url) != -1) { // the url is already indexed in UriMapping
-        urlNode.set(fst.getID(url));
-        linkNodes = new ArrayList<IntWritable>();
-        Set<IntWritable> linkUrlSet = new HashSet<IntWritable>();
+        KEY.set(fst.getID(url));
+        IntAVLTreeSet linkUrlSet = new IntAVLTreeSet();
         if (links != null) {
           for (Element link : links) {
             String linkUrl = link.attr("abs:href");
-            if (fst.getID(linkUrl) != -1) { // linkUrl is already indexed
-              linkUrlSet.add(new IntWritable(fst.getID(linkUrl)));
+            if (fst.getID(linkUrl) != -1) { // link already exists
+              linkUrlSet.add(fst.getID(linkUrl));
             }
           }
-          boolean emitFlag = false;
-          for (IntWritable linkID : linkUrlSet) {
-            linkNodes.add(linkID);
-            emitFlag = true;
-            context.getCounter(Records.LINK_COUNT).increment(1);
-          }
-          if (emitFlag == false) { // contain no links which are indexed in UriMapping
-            context.getCounter(Records.LINK_COUNT).increment(1);
+
+          if (linkUrlSet.size() == 0) {
+            // Emit empty entry even if there aren't any outgoing links
+            VALUE.set("");
+            context.write(KEY, VALUE);
+            return;
           }
 
-        } else { // webpage without outgoing links
-          context.getCounter(Records.LINK_COUNT).increment(1);
+          VALUE.set(JOINER.join(linkUrlSet));
+          context.getCounter(Records.LINK_COUNT).increment(linkUrlSet.size());
+          context.write(KEY, VALUE);
         }
-        context.write(urlNode, linkNodes);
       }
     }
   }
@@ -154,16 +143,14 @@ public class ExtractLinks extends Configured implements Tool {
   /**
    * Creates an instance of this tool.
    */
-  public ExtractLinks() {
-  }
+  public ExtractLinks() {}
 
   private static final String INPUT = "input";
   private static final String OUTPUT = "output";
   private static final String URI_MAPPING = "uriMapping";
-  private static final String NUM_REDUCERS = "numReducers";
-  private static final String BEGIN="begin";
-  private static final String END="end";
-  private static String beginDate=null, endDate=null;
+  private static final String BEGIN = "begin";
+  private static final String END = "end";
+  private static String beginDate = null, endDate = null;
 
   /**
    * Runs this tool.
@@ -172,18 +159,16 @@ public class ExtractLinks extends Configured implements Tool {
   public int run(String[] args) throws Exception {
     Options options = new Options();
 
-    options.addOption(OptionBuilder.withArgName("path").hasArg().withDescription("input path")
-        .create(INPUT));
-    options.addOption(OptionBuilder.withArgName("path").hasArg().withDescription("output path")
-        .create(OUTPUT));
+    options.addOption(OptionBuilder.withArgName("path").hasArg()
+        .withDescription("input path").create(INPUT));
+    options.addOption(OptionBuilder.withArgName("path").hasArg()
+        .withDescription("output path").create(OUTPUT));
     options.addOption(OptionBuilder.withArgName("path").hasArg()
         .withDescription("uri mapping file path").create(URI_MAPPING));
-    options.addOption(OptionBuilder.withArgName("num").hasArg()
-        .withDescription("number of reducers").create(NUM_REDUCERS));
-    options.addOption(OptionBuilder.withArgName("path").hasArg().withDescription("begin date (optional)")
-        .create(BEGIN));
-    options.addOption(OptionBuilder.withArgName("path").hasArg().withDescription("end date (optional)")
-        .create(END));
+    options.addOption(OptionBuilder.withArgName("path").hasArg()
+        .withDescription("begin date (optional)").create(BEGIN));
+    options.addOption(OptionBuilder.withArgName("path").hasArg()
+        .withDescription("end date (optional)").create(END));
 
     CommandLine cmdline;
     CommandLineParser parser = new GnuParser();
@@ -204,39 +189,36 @@ public class ExtractLinks extends Configured implements Tool {
       return -1;
     }
 
+    FileSystem fs = FileSystem.get(getConf());
+
     String inputPath = cmdline.getOptionValue(INPUT);
     String outputPath = cmdline.getOptionValue(OUTPUT);
-    String mappingPath = cmdline.getOptionValue(URI_MAPPING);
-    int reduceTasks = cmdline.hasOption(NUM_REDUCERS) ? Integer.parseInt(cmdline
-        .getOptionValue(NUM_REDUCERS)) : 1;
+    Path mappingPath = new Path(cmdline.getOptionValue(URI_MAPPING));
 
     LOG.info("Tool: " + ExtractLinks.class.getSimpleName());
     LOG.info(" - input path: " + inputPath);
     LOG.info(" - output path: " + outputPath);
-    LOG.info(" - mapping file path:" + mappingPath);
-    LOG.info(" - number of reducers: " + reduceTasks);
-    if(cmdline.hasOption(BEGIN)){
+    LOG.info(" - mapping file path: " + mappingPath);
+
+    if (cmdline.hasOption(BEGIN)) {
       beginDate = cmdline.getOptionValue(BEGIN);
       LOG.info(" - begin date: " + beginDate);
     }
-    if(cmdline.hasOption(END)){
+    if (cmdline.hasOption(END)) {
       endDate = cmdline.getOptionValue(END);
       LOG.info(" - end date: " + endDate);
     }
-    
-    
-    Job job = new Job(getConf(), ExtractLinks.class.getSimpleName());
+
+    if (!fs.exists(mappingPath)) {
+      throw new Exception("mappingPath doesn't exist: " + mappingPath);
+    }
+
+    Job job = Job.getInstance(getConf(), ExtractLinks.class.getSimpleName());
     job.setJarByClass(ExtractLinks.class);
 
-    // Pass in the class name as a String; this is makes the mapper general
-    // in being able to load any collection of Indexable objects that has
-    // url_id/url mapping specified by a UriMapping object.
-    //job.getConfiguration().set("mapred.job.tracker", "local");
-    //job.getConfiguration().set("fs.default.name", "local");
     job.getConfiguration().set("UriMappingClass", UriMapping.class.getCanonicalName());
-    // Put the mapping file in the distributed cache so each map worker will
-    // have it.
-    DistributedCache.addCacheFile(new URI(mappingPath), job.getConfiguration());
+    // Put the mapping file in the distributed cache so each map worker will have it.
+    job.addCacheFile(mappingPath.toUri());
 
     job.setNumReduceTasks(0); // no reducers
 
@@ -252,7 +234,7 @@ public class ExtractLinks extends Configured implements Tool {
 
     // Delete the output directory if it exists already.
     Path outputDir = new Path(outputPath);
-    FileSystem.get(job.getConfiguration()).delete(outputDir, true);
+    fs.delete(outputDir, true);
 
     long startTime = System.currentTimeMillis();
     job.waitForCompletion(true);
