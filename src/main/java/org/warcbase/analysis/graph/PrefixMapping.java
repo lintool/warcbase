cfb89d2d3379938625900d5aed3db8c26b75894f
Jeffyrao <raojinfeng@gmail.com>
2014-06-11 23:10:18 -0400

    reformat Jinfeng's code

--- src/main/java/org/warcbase/analysis/graph/PrefixMapping.java
+++ src/main/java/org/warcbase/analysis/graph/PrefixMapping.java
@@ -10,69 +10,82 @@ import java.util.List;
 import org.warcbase.data.UriMapping;
 
 import au.com.bytecode.opencsv.CSVReader;
-public class PrefixMapping{
+
+public class PrefixMapping {
   public class PrefixNode {
     int id;
     String url;
     int startPos;
     int endPos;
-    
-    public PrefixNode(int id, String url, int startPos, int endPos){
+
+    public PrefixNode(int id, String url, int startPos, int endPos) {
       this.id = id;
       this.url = url;
       this.startPos = startPos;
       this.endPos = endPos;
     }
-    
-    public int getId(){ return id; }
-    public String getUrl(){ return url; }
-    public int getStartPos(){ return startPos; }
-    public int getEndPos(){ return endPos; }
+
+    public int getId() {
+      return id;
+    }
+
+    public String getUrl() {
+      return url;
+    }
+
+    public int getStartPos() {
+      return startPos;
+    }
+
+    public int getEndPos() {
+      return endPos;
+    }
   }
-  
-  public static ArrayList<PrefixNode> loadPrefix(String prefixFile, UriMapping map) throws IOException{
+
+  public static ArrayList<PrefixNode> loadPrefix(String prefixFile, UriMapping map)
+      throws IOException {
     PrefixMapping instance = new PrefixMapping();
-    final Comparator<PrefixNode> comparator = new Comparator<PrefixNode>(){
+    final Comparator<PrefixNode> comparator = new Comparator<PrefixNode>() {
       @Override
-      public int compare(PrefixNode n1, PrefixNode n2){
-        if(n1.startPos > n2.startPos){
+      public int compare(PrefixNode n1, PrefixNode n2) {
+        if (n1.startPos > n2.startPos) {
           return 1;
-        }else if( n1.startPos == n2.startPos ){
+        } else if (n1.startPos == n2.startPos) {
           return 0;
-        }else{
+        } else {
           return -1;
         }
       }
     };
     ArrayList<PrefixNode> prefixes = new ArrayList<PrefixNode>();
-    CSVReader reader = new CSVReader(new FileReader(prefixFile),',');
+    CSVReader reader = new CSVReader(new FileReader(prefixFile), ',');
     reader.readNext();
     String line;
     String[] record = null;
-    while((record = reader.readNext())!=null){
+    while ((record = reader.readNext()) != null) {
       int id = Integer.valueOf(record[0]);
       String url = record[1];
       List<String> results = map.prefixSearch(url);
-      int[] boundary = map.getIdRange(results.get(0), results.get(results.size()-1));
+      int[] boundary = map.getIdRange(results.get(0), results.get(results.size() - 1));
       PrefixNode node = instance.new PrefixNode(id, url, boundary[0], boundary[1]);
       prefixes.add(node);
     }
-    Collections.sort(prefixes,comparator);
+    Collections.sort(prefixes, comparator);
     reader.close();
     return prefixes;
   }
-  
-  public int getPrefixId(int id, ArrayList<PrefixNode> prefixes){
-    int start = 0, end = prefixes.size()-1;
+
+  public int getPrefixId(int id, ArrayList<PrefixNode> prefixes) {
+    int start = 0, end = prefixes.size() - 1;
     int mid;
-    while(start<=end){
-      mid = (start+end)/2;
-      if(prefixes.get(mid).getStartPos() <= id && prefixes.get(mid).getEndPos() >= id){
+    while (start <= end) {
+      mid = (start + end) / 2;
+      if (prefixes.get(mid).getStartPos() <= id && prefixes.get(mid).getEndPos() >= id) {
         return prefixes.get(mid).getId();
-      }else if(prefixes.get(mid).getStartPos() > id){
-        end = mid-1;
-      }else{
-        start = mid+1;
+      } else if (prefixes.get(mid).getStartPos() > id) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
       }
     }
     return -1;
