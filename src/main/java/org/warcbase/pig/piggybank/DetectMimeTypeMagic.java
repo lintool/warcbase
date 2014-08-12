a14cda2217fabae491f8758da9aba483b998b6d5
lintool <jimmylin@umd.edu>
2014-08-12 17:15:30 -0400

    Commented out LibmagicJnaWrapper functionality because the jar isn't generally available.

--- src/main/java/org/warcbase/pig/piggybank/DetectMimeTypeMagic.java
+++ src/main/java/org/warcbase/pig/piggybank/DetectMimeTypeMagic.java
@@ -2,7 +2,6 @@ package org.warcbase.pig.piggybank;
 
 import org.apache.pig.EvalFunc;
 import org.apache.pig.data.Tuple;
-import org.opf_labs.LibmagicJnaWrapper;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -12,7 +11,7 @@ public class DetectMimeTypeMagic extends EvalFunc<String> {
 
     @Override
     public String exec(Tuple input) throws IOException {
-        String mimeType;
+        String mimeType = null;
 
         if (input == null || input.size() == 0 || input.get(0) == null) {
             return "N/A";
@@ -23,13 +22,16 @@ public class DetectMimeTypeMagic extends EvalFunc<String> {
         InputStream is = new ByteArrayInputStream(content.getBytes());
         if (content.isEmpty()) return "EMPTY";
 
-        LibmagicJnaWrapper jnaWrapper = new LibmagicJnaWrapper();
-        jnaWrapper.load(magicFile);
+        // I'm commenting this out because the jar isn't actually published anywhere...
+        // @lintool 2014/08/12
+
+        //org.opf_labs.LibmagicJnaWrapper jnaWrapper = new LibmagicJnaWrapper();
+        //jnaWrapper.load(magicFile);
+
         //jnaWrapper.load("/usr/local/Cellar/libmagic/5.15/share/misc/magic.mgc"); // Mac OS X with Homebrew
         //jnaWrapper.load("/usr/share/file/magic.mgc"); // CentOS
 
-        mimeType = jnaWrapper.getMimeType(is);
-
+        //mimeType = jnaWrapper.getMimeType(is);
         return mimeType;
     }
 }
