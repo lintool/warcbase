377a3e3a429f835fcdf9f79a2ad09673ace6ff50
lintool <jimmylin@umd.edu>
2014-08-14 19:31:46 -0400

    Better handling of ARC parse errors.

--- src/main/java/org/warcbase/ingest/IngestFiles.java
+++ src/main/java/org/warcbase/ingest/IngestFiles.java
@@ -59,8 +59,7 @@ public class IngestFiles {
       out.write(scratchbuffer, 0, write);
     }
     if (enforceLength && tot != recordLength) {
-      // throw exception if desired for read vs. declared mismatches
-      throw new IOException("Read " + tot + " but expected " + recordLength);
+      LOG.error("Read " + tot + " bytes but expected " + recordLength + " bytes. Continuing...");
     }
 
     return tot;
@@ -125,12 +124,15 @@ public class IngestFiles {
         }
 
         if (cnt % 10000 == 0 && cnt > 0) {
-          LOG.info("Ingested " + cnt + " records into Hbase.");
+          LOG.info("Ingested " + cnt + " records into HBase.");
         }
       }
     } catch (Exception e) {
       LOG.error("Error ingesting file: " + inputArcFile);
       e.printStackTrace();
+    } catch (OutOfMemoryError e) {
+      LOG.error("Encountered OutOfMemoryError ingesting file: " + inputArcFile);
+      LOG.error("Attempting to continue...");
     } finally {
       if (reader != null)
         try {
