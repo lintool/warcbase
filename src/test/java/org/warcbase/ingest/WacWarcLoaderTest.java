4798a4314b6e2af154a06bb96b071481363ed422
lintool <jimmylin@umd.edu>
2014-08-29 21:10:50 -0400

    Figured out how to extract MIME type and date from WARC.

--- src/test/java/org/warcbase/ingest/WacWarcLoaderTest.java
+++ src/test/java/org/warcbase/ingest/WacWarcLoaderTest.java
@@ -7,7 +7,10 @@ import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -16,6 +19,7 @@ import org.archive.io.ArchiveRecordHeader;
 import org.archive.io.warc.WARCReader;
 import org.archive.io.warc.WARCReaderFactory;
 import org.archive.io.warc.WARCRecord;
+import org.archive.util.ArchiveUtils;
 import org.junit.Test;
 import org.warcbase.data.WarcRecordUtils;
 
@@ -26,6 +30,7 @@ import com.google.common.io.Resources;
 
 public class WacWarcLoaderTest {
   private static final Log LOG = LogFactory.getLog(WacWarcLoaderTest.class);
+  private static final SimpleDateFormat DATE_WARC = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
 
   @Test
   public void testReader() throws Exception {
@@ -45,6 +50,22 @@ public class WacWarcLoaderTest {
       byte[] contents = WarcRecordUtils.getContent(r);
       int len = (int) header.getContentLength();
       assertEquals(len, contents.length);
+
+      // This is how you extract the date
+      @SuppressWarnings("unused")
+      String digit14Date = ArchiveUtils.get14DigitDate(DATE_WARC.parse(header.getDate()));
+
+      // This is a somewhat janky way to get the MIME type of the response.
+      // Note that this is different from the "Content-Type" in the WARC header.
+      if (header.getHeaderValue("WARC-Type").equals("response") &&
+          header.getUrl().startsWith("http://")) {
+        Pattern pattern = Pattern.compile("Content-Type: ([^\\s]+)");
+        Matcher matcher = pattern.matcher(new String(contents));
+        if (matcher.find()) {
+          @SuppressWarnings("unused")
+          String mimeType = matcher.group(1).replaceAll(";$", "");
+        }
+      }
 
       cnt++;
     }
