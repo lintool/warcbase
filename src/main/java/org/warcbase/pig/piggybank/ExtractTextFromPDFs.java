c49c58f76ed3ca4ae0f218541e07bec0de4e4d23
rwolniak <wolnicat@gmail.com>
2014-12-08 18:38:34 -0500

    ExtractTextFromPDFs updated

--- src/main/java/org/warcbase/pig/piggybank/ExtractTextFromPDFs.java
+++ src/main/java/org/warcbase/pig/piggybank/ExtractTextFromPDFs.java
@@ -29,26 +29,30 @@ public class ExtractTextFromPDFs extends EvalFunc<String> {
           return "N/A";
       }
       
+      //prepare input for use with Tika
       DataByteArray dba = (DataByteArray)input.get(0);
-      
       InputStream is = new ByteArrayInputStream(dba.get());
       
+      //set up Tika constructs
       ContentHandler contenthandler = new BodyContentHandler(Integer.MAX_VALUE);
       Metadata metadata = new Metadata();
 
       try {
+        //parse the bytestream of the PDF into plain text
         pdfparser.parse(is, contenthandler, metadata, new ParseContext());
       } catch (SAXException | TikaException e) {
-        // TODO Auto-generated catch block
         e.printStackTrace();
       }
       
+      //append the PDF's plain text to the string
       pdfText.append(contenthandler.toString());
 
       //close the input stream
       if(is != null){
         is.close();
       }
+      
+      //return the plain text of the PDF
       return pdfText.toString();
   }
   
