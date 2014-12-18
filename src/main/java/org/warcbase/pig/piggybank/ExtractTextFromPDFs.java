5c2540099370fb9e0c1a883791dd3d17b5fcac58
lintool <jimmylin@umd.edu>
2014-12-18 08:20:23 -0500

    Reformatting.

--- src/main/java/org/warcbase/pig/piggybank/ExtractTextFromPDFs.java
+++ src/main/java/org/warcbase/pig/piggybank/ExtractTextFromPDFs.java
@@ -16,44 +16,41 @@ import org.apache.tika.sax.BodyContentHandler;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
-
 public class ExtractTextFromPDFs extends EvalFunc<String> {
-  
   private Parser pdfparser = new PDFParser();
-  
+
   @Override
   public String exec(Tuple input) throws IOException {
-      StringBuilder pdfText = new StringBuilder();
+    StringBuilder pdfText = new StringBuilder();
 
-      if (input == null || input.size() == 0 || input.get(0) == null) {
-          return "N/A";
-      }
-      
-      //prepare input for use with Tika
-      DataByteArray dba = (DataByteArray)input.get(0);
-      InputStream is = new ByteArrayInputStream(dba.get());
-      
-      //set up Tika constructs
-      ContentHandler contenthandler = new BodyContentHandler(Integer.MAX_VALUE);
-      Metadata metadata = new Metadata();
+    if (input == null || input.size() == 0 || input.get(0) == null) {
+      return "N/A";
+    }
 
-      try {
-        //parse the bytestream of the PDF into plain text
-        pdfparser.parse(is, contenthandler, metadata, new ParseContext());
-      } catch (SAXException | TikaException e) {
-        e.printStackTrace();
-      }
-      
-      //append the PDF's plain text to the string
-      pdfText.append(contenthandler.toString());
+    // prepare input for use with Tika
+    DataByteArray dba = (DataByteArray) input.get(0);
+    InputStream is = new ByteArrayInputStream(dba.get());
 
-      //close the input stream
-      if(is != null){
-        is.close();
-      }
-      
-      //return the plain text of the PDF
-      return pdfText.toString();
+    // set up Tika constructs
+    ContentHandler contenthandler = new BodyContentHandler(Integer.MAX_VALUE);
+    Metadata metadata = new Metadata();
+
+    try {
+      // parse the bytestream of the PDF into plain text
+      pdfparser.parse(is, contenthandler, metadata, new ParseContext());
+    } catch (SAXException | TikaException e) {
+      e.printStackTrace();
+    }
+
+    // append the PDF's plain text to the string
+    pdfText.append(contenthandler.toString());
+
+    // close the input stream
+    if (is != null) {
+      is.close();
+    }
+
+    // return the plain text of the PDF
+    return pdfText.toString();
   }
-  
 }
