1c8cd5c78b1fa439ca49a336815a800d939c9ca6
lintool <jimmylin@uwaterloo.ca>
2016-03-05 11:54:49 -0500

    Added method to extract text.

--- src/main/scala/org/warcbase/util/TweetJsonFunctions.scala
+++ src/main/scala/org/warcbase/util/TweetJsonFunctions.scala
@@ -8,6 +8,7 @@ object TweetJsonFunctions {
 
     def id(): String = (tweet \ "id_str").extract[String]
     def createdAt(): String = (tweet \ "created_at").extract[String]
+    def text(): String = (tweet \ "text").extract[String]
     def lang: String = (tweet \ "lang").extract[String]
 
     def username(): String = (tweet \ "user" \ "screen_name").extract[String]
