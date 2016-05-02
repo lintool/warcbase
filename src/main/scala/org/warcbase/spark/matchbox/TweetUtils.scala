03c93f22c0142387845c7562bba4fe79763bff9a
lintool <jimmylin@umd.edu>
2016-05-01 20:46:00 -0400

    More robust error handling.

--- src/main/scala/org/warcbase/spark/matchbox/TweetUtils.scala
+++ src/main/scala/org/warcbase/spark/matchbox/TweetUtils.scala
@@ -6,15 +6,15 @@ object TweetUtils {
   implicit class JsonTweet(tweet: JValue) {
     implicit lazy val formats = org.json4s.DefaultFormats
 
-    def id(): String = (tweet \ "id_str").extract[String]
-    def createdAt(): String = (tweet \ "created_at").extract[String]
-    def text(): String = (tweet \ "text").extract[String]
-    def lang: String = (tweet \ "lang").extract[String]
+    def id(): String = try { (tweet \ "id_str").extract[String] } catch { case e: Exception => null}
+    def createdAt(): String = try { (tweet \ "created_at").extract[String] } catch { case e: Exception => null}
+    def text(): String = try { (tweet \ "text").extract[String] } catch { case e: Exception => null}
+    def lang: String = try { (tweet \ "lang").extract[String] } catch { case e: Exception => null}
 
-    def username(): String = (tweet \ "user" \ "screen_name").extract[String]
-    def isVerifiedUser(): Boolean = (tweet \ "user" \ "screen_name").extract[String] == "false"
+    def username(): String = try { (tweet \ "user" \ "screen_name").extract[String] } catch { case e: Exception => null}
+    def isVerifiedUser(): Boolean = try { (tweet \ "user" \ "screen_name").extract[String] == "false" } catch { case e: Exception => false}
 
-    def followerCount: Int = (tweet \ "user" \ "followers_count").extract[Int]
-    def friendCount: Int = (tweet \ "user" \ "friends_count").extract[Int]
+    def followerCount: Int = try { (tweet \ "user" \ "followers_count").extract[Int] } catch { case e: Exception => 0}
+    def friendCount: Int = try { (tweet \ "user" \ "friends_count").extract[Int] } catch { case e: Exception => 0}
   }
 }
