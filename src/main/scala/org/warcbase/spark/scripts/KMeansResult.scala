96c329b8e93f37690d24a8a884c279d455b94bc0
Youngbin Kim <ykim828@hotmail.com>
2016-05-15 23:05:39 -0400

    extract top N words

--- src/main/scala/org/warcbase/spark/scripts/KMeansResult.scala
+++ src/main/scala/org/warcbase/spark/scripts/KMeansResult.scala
@@ -1,29 +1,37 @@
 package org.warcbase.spark.scripts
 
+import org.apache.spark.SparkContext
 import org.apache.spark.mllib.clustering.{LDA, KMeansModel}
+import org.apache.spark.mllib.feature.HashingTF
 import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.clustering.KMeansModel
 import org.apache.spark.mllib.linalg.Vector
 
-/**
-  * Created by youngbinkim on 5/15/16.
-  */
-class KMeansResult(clusters: KMeansModel, tfidf: RDD[Vector], numTopics: Int = 3) {
-  implicit class KMeansResult(clusters: KMeansModel) {
-    def computeLDA(path: String): Unit = {
-      for (i <- 1 to clusters.k) {
-        val cluster = tfidf.filter(v => clusters.predict(v) == 1).persist()
-        println(s"cluster size ${cluster.count()}")
-        val corpus = cluster.zipWithIndex.map(_.swap).cache()
-        val ldaModel = new LDA().setK(numTopics).run(corpus)
-        println("Learned topics (as distributions over vocab of " + ldaModel.vocabSize + " words):")
-        val topics = ldaModel.topicsMatrix
-        for (topic <- Range(0, 3)) {
-          print("Topic " + topic + ":")
-          for (word <- Range(0, ldaModel.vocabSize)) { print(" " + topics(word, topic)); }
-          println()
-        }
+
+class KMeansResult(clusters: KMeansModel, tfidf: RDD[Vector], lemmatized: RDD[Seq[String]]) {
+  lazy val indexToTerm = lemmatized.flatMap(seq => seq.map(f=>f)).map(s=>(hashingTF.indexOf(s), s)).cache()
+  val hashingTF = new HashingTF()
+  
+  def computeLDA(output: String, numTopics: Int = 3): Unit = {
+    for (i <- 1 to clusters.k) {
+      val cluster = tfidf.filter(v => clusters.predict(v) == 1).persist()
+      println(s"cluster size ${cluster.count()}")
+      val corpus = cluster.zipWithIndex.map(_.swap).cache()
+      val ldaModel = new LDA().setK(numTopics).run(corpus)
+      println("Learned topics (as distributions over vocab of " + ldaModel.vocabSize + " words):")
+      val topics = ldaModel.topicsMatrix
+      for (topic <- Range(0, numTopics)) {
+        print("Topic " + topic + ":")
+        for (word <- Range(0, ldaModel.vocabSize)) { print(" " + topics(word, topic)); }
+        println()
       }
     }
+  }
+
+  def topNWords(output: String, limit: Int = 10, sc: SparkContext) = {
+    clusters.clusterCenters.foreach(v => {
+      val topWords = sc.parallelize(v.toArray).zipWithIndex.takeOrdered(limit)(Ordering[Double].reverse.on(x=>x._1));
+      sc.parallelize(topWords).map{ case (k, i) => (k, indexToTerm.lookup(i.toInt))}.saveAsTextFile(output)
+    })
   }
 }
--- src/main/scala/org/warcbase/spark/scripts/KMeansRunner.scala
+++ src/main/scala/org/warcbase/spark/scripts/KMeansRunner.scala
@@ -1,33 +1,23 @@
 package org.warcbase.spark.scripts
 
-import java.util.Properties
-import edu.stanford.nlp.ling.CoreAnnotations.{LemmaAnnotation, SentencesAnnotation, TokensAnnotation}
-import edu.stanford.nlp.pipeline.{Annotation, StanfordCoreNLP}
-import org.apache.spark.graphx.Graph
-import org.apache.spark.mllib.clustering.{LDA, KMeansModel, KMeans}
-import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.sql.{DataFrame, SQLContext}
-
-
-import org.tartarus.snowball.ext.EnglishStemmer
 import org.apache.spark.SparkContext
-import edu.stanford.nlp.process._
 import org.apache.spark.broadcast.Broadcast
-import org.apache.spark.mllib.feature.{IDF, HashingTF}
-import org.apache.spark.mllib.linalg
+import org.apache.spark.mllib.clustering.KMeans
+import org.apache.spark.mllib.feature.{HashingTF, IDF}
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SQLContext
 import org.jsoup.Jsoup
+import org.tartarus.snowball.ext.EnglishStemmer
 import org.warcbase.spark.archive.io.ArchiveRecord
 import org.warcbase.spark.rdd.RecordRDD._
-import scala.collection.JavaConversions._
-import scala.collection.mutable.{ListBuffer, ArrayBuffer}
+
+import scala.collection.mutable.ListBuffer
 
 object KMeansRunner {
   def apply(records: RDD[ArchiveRecord], sc: SparkContext, k: Int = 20, numIterations: Int = 20) = {
     val stopwords = sc.broadcast(Set("a", "as", "able", "about", "above", "according", "accordingly", "across", "actually", "after", "afterwards", "again", "against", "aint", "all", "allow", "allows", "almost", "alone", "along", "already", "also", "although", "always", "am", "among", "amongst", "an", "and", "another", "any", "anybody", "anyhow", "anyone", "anything", "anyway", "anyways", "anywhere", "apart", "appear", "appreciate", "appropriate", "are", "arent", "around", "as", "aside", "ask", "asking", "associated", "at", "available", "away", "awfully", "be", "became", "because", "become", "becomes", "becoming", "been", "before", "beforehand", "behind", "being", "believe", "below", "beside", "besides", "best", "better", "between", "beyond", "both", "brief", "but", "by", "cmon", "cs", "came", "can", "cant", "cannot", "cant", "cause", "causes", "certain", "certainly", "changes", "clearly", "co", "com", "come", "comes", "concerning", "consequently", "consider", "considering", "contain", "containing", "contains", "corresponding", "could", "couldnt", "course", "currently", "definitely", "described", "despite", "did", "didnt", "different", "do", "does", "doesnt", "doing", "dont", "done", "down", "downwards", "during", "each", "edu", "eg", "eight", "either", "else", "elsewhere", "enough", "entirely", "especially", "et", "etc", "even", "ever", "every", "everybody", "everyone", "everything", "everywhere", "ex", "exactly", "example", "except", "far", "few", "ff", "fifth", "first", "five", "followed", "following", "follows", "for", "former", "formerly", "forth", "four", "from", "further", "furthermore", "get", "gets", "getting", "given", "gives", "go", "goes", "going", "gone", "got", "gotten", "greetings", "had", "hadnt", "happens", "hardly", "has", "hasnt", "have", "havent", "having", "he", "hes", "hello", "help", "hence", "her", "here", "heres", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "hi", "him", "himself", "his", "hither", "hopefully", "how", "howbeit", "however", "i", "id", "ill", "im", "ive", "ie", "if", "ignored", "immediate", "in", "inasmuch", "inc", "indeed", "indicate", "indicated", "indicates", "inner", "insofar", "instead", "into", "inward", "is", "isnt", "it", "itd", "itll", "its", "its", "itself", "just", "keep", "keeps", "kept", "know", "knows", "known", "last", "lately", "later", "latter", "latterly", "least", "less", "lest", "let", "lets", "like", "liked", "likely", "little", "look", "looking", "looks", "ltd", "mainly", "many", "may", "maybe", "me", "mean", "meanwhile", "merely", "might", "more", "moreover", "most", "mostly", "much", "must", "my", "myself", "name", "namely", "nd", "near", "nearly", "necessary", "need", "needs", "neither", "never", "nevertheless", "new", "next", "nine", "no", "nobody", "non", "none", "noone", "nor", "normally", "not", "nothing", "novel", "now", "nowhere", "obviously", "of", "off", "often", "oh", "ok", "okay", "old", "on", "once", "one", "ones", "only", "onto", "or", "other", "others", "otherwise", "ought", "our", "ours", "ourselves", "out", "outside", "over", "overall", "own", "particular", "particularly", "per", "perhaps", "placed", "please", "plus", "possible", "presumably", "probably", "provides", "que", "quite", "qv", "rather", "rd", "re", "really", "reasonably", "regarding", "regardless", "regards", "relatively", "respectively", "right", "said", "same", "saw", "say", "saying", "says", "second", "secondly", "see", "seeing", "seem", "seemed", "seeming", "seems", "seen", "self", "selves", "sensible", "sent", "serious", "seriously", "seven", "several", "shall", "she", "should", "shouldnt", "since", "six", "so", "some", "somebody", "somehow", "someone", "something", "sometime", "sometimes", "somewhat", "somewhere", "soon", "sorry", "specified", "specify", "specifying", "still", "sub", "such", "sup", "sure", "ts", "take", "taken", "tell", "tends", "th", "than", "thank", "thanks", "thanx", "that", "thats", "thats", "the", "their", "theirs", "them", "themselves", "then", "thence", "there", "theres", "thereafter", "thereby", "therefore", "therein", "theres", "thereupon", "these", "they", "theyd", "theyll", "theyre", "theyve", "think", "third", "this", "thorough", "thoroughly", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "took", "toward", "towards", "tried", "tries", "truly", "try", "trying", "twice", "two", "un", "under", "unfortunately", "unless", "unlikely", "until", "unto", "up", "upon", "us", "use", "used", "useful", "uses", "using", "usually", "value", "various", "very", "via", "viz", "vs", "want", "wants", "was", "wasnt", "way", "we", "wed", "well", "were", "weve", "welcome", "well", "went", "were", "werent", "what", "whats", "whatever", "when", "whence", "whenever", "where", "wheres", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whos", "whoever", "whole", "whom", "whose", "why", "will", "willing", "wish", "with", "within", "without", "wont", "wonder", "would", "would", "wouldnt", "yes", "yet", "you", "youd", "youll", "youre", "youve", "your", "yours", "yourself", "yourselves", "zero"))
 
     val rec = records.keepValidPages().persist()
-
     val lemmatized = rec.mapPartitions(r => {
       val stemmer = new EnglishStemmer()
       stemmer.stem()
@@ -36,24 +26,8 @@ object KMeansRunner {
 
     val tfidf = getTfIdf(lemmatized).cache()
 
-    new KMeansResult(KMeans.train(tfidf, k, numIterations), tfidf)
+    new KMeansResult(KMeans.train(tfidf, k, numIterations), tfidf, lemmatized)
   }
-
-  def getLemmas(text: String, stemmer: EnglishStemmer, stopwords: Broadcast[Set[String]]): Seq[String] = {
-    val lemmas = new ListBuffer[String]()
-    val words = text.split(" ")
-    for (word <- words){
-      stemmer.setCurrent(word.toLowerCase.replaceAll("[^\\p{L}\\p{Nd}]+", ""))
-      stemmer.stem()
-      val lemma = stemmer.getCurrent()
-      println(lemma)
-      if (lemma.length > 2 && !stopwords.value.contains(lemma)) {
-        lemmas += lemma
-      }
-    }
-    lemmas.toList
-  }
-
   def getLemmas(q: ArchiveRecord, stemmer: EnglishStemmer, stopwords: Broadcast[Set[String]]): Seq[String] = {
     val text = Jsoup.parse(q.getContentString).select("body").first().text()
     val lemmas = new ListBuffer[String]()
