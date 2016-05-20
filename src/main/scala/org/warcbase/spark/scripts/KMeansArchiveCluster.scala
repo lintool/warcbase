7e0732e1a37fa4f2830edf1319aa73dde4de1b20
Youngbin Kim <ykim828@hotmail.com>
2016-05-20 01:38:16 -0400

    more parallelization ?

--- src/main/scala/org/warcbase/spark/scripts/KMeansArchiveCluster.scala
+++ src/main/scala/org/warcbase/spark/scripts/KMeansArchiveCluster.scala
@@ -5,7 +5,6 @@ import org.apache.spark.mllib.feature.HashingTF
 import org.apache.spark.mllib.linalg.{Vector, Vectors}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{HashPartitioner, SparkContext}
-import org.warcbase.spark.archive.io.ArchiveRecord
 
 import scala.collection.mutable.ArrayBuffer
 
@@ -18,22 +17,22 @@ class KMeansArchiveCluster(clusters: KMeansModel, tfidf: RDD[Vector], lemmatized
   lazy val clusterRdds = getClusterRdds()
 
   private def getClusterRdds() = {
-    val rdds = new ArrayBuffer[RDD[(Vector, String)]]
+    val rdds = new ArrayBuffer[(Int, RDD[(Vector, String)])]
     val merged = tfidf.zip(rec).map(r=>(r._1, r._2))
     for (i <- 0 to clusters.k-1) {
-      rdds += merged.filter(v => clusters.predict(v._1) == i).persist()
+      rdds += Pair(i, merged.filter(v => clusters.predict(v._1) == i).persist())
     }
     rdds
   }
 
   def getSampleDocs(sc: SparkContext, numDocs: Int=10): RDD[(Int, String)] ={
     var res:RDD[(Int, String)] = sc.emptyRDD[(Int, String)]
-    for (i <- 0 to clusters.k-1) {
-      val cluster = clusterRdds(i)
-      val p= clusters.clusterCenters(i)
-      val docs = cluster.map(r => (Vectors.sqdist(p, r._1), r._2)).takeOrdered(numDocs)(Ordering[Double].on(x=>x._1))
-      res = res.union(sc.parallelize(docs).map(r => (i, r._2)))
-    }
+    clusterRdds.par.foreach(c => {
+      val cluster = c._2
+      val p = clusters.clusterCenters(c._1)
+      val docs = cluster.map(r => (Vectors.sqdist(p, r._1), r._2)).takeOrdered(numDocs)(Ordering[Double].on(x => x._1))
+      res = res.union(sc.parallelize(docs).map(r => (c._1, r._2)))
+    })
     res
   }
 
@@ -44,8 +43,8 @@ class KMeansArchiveCluster(clusters: KMeansModel, tfidf: RDD[Vector], lemmatized
 
   def computeLDA(output: String, sc: SparkContext, numTopics: Int = 3, numWordsPerTopic: Int = 10) = {
     var res:RDD[(Int, (Long, Seq[String], Double))] = sc.emptyRDD[(Int, (Long, Seq[String], Double))]
-    for (i <- 0 to clusters.k-1) {
-      val cluster = tfidf.filter(v => clusters.predict(v) == i).persist()
+    clusterRdds.par.foreach(c => {
+      val cluster = c._2.map(x=>x._1)
       println(s"cluster size ${cluster.count()}")
       val corpus = cluster.zipWithIndex.map(_.swap).cache()
       val ldaModel = new LDA().setK(numTopics).run(corpus)
@@ -55,8 +54,8 @@ class KMeansArchiveCluster(clusters: KMeansModel, tfidf: RDD[Vector], lemmatized
       val topicWords = topicRdd.zipWithIndex().map(_.swap).flatMap(r => r._2._1.map(word => (r._1, word)))
       val topicScores = topicRdd.flatMap(r => r._2.map(word=>word))
       val topics = topicWords.zip(topicScores)
-      res = res.union(topics.map(r => (i, (r._1._1, r._1._2, r._2))))
-    }
+      res = res.union(topics.map(r => (c._1, (r._1._1, r._1._2, r._2))))
+    })
     res.partitionBy(new HashPartitioner(clusters.k)).map(r=>r._2).saveAsTextFile(output)
     this
   }
